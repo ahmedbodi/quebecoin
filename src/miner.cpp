8b37d9f0316920b0e09b22b71de32ea272ccb06c
Ahmed Bodiwala <ahmed_bodi@msn.com>
2019-05-31 05:03:26 +0000

    Dev Fee Fixes and Icons

--- src/miner.cpp
+++ src/miner.cpp
@@ -173,8 +173,12 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
     coinbaseTx.vout.resize(2);
     coinbaseTx.vout[0].scriptPubKey = scriptPubKeyIn;
     coinbaseTx.vout[0].nValue = (nFees + GetBlockSubsidy(nHeight, chainparams.GetConsensus())) / 2;
+
+    /** Only Needed to pass Dev Fee Tests since mining is done via GBT */
     coinbaseTx.vout[1].scriptPubKey = GetFoundationScript();
     coinbaseTx.vout[1].nValue = (nFees + GetBlockSubsidy(nHeight, chainparams.GetConsensus())) / 2;
+    /**  End Dev Fee **/
+
     coinbaseTx.vin[0].scriptSig = CScript() << nHeight << OP_0;
     pblock->vtx[0] = MakeTransactionRef(std::move(coinbaseTx));
     pblocktemplate->vchCoinbaseCommitment = GenerateCoinbaseCommitment(*pblock, pindexPrev, chainparams.GetConsensus());
